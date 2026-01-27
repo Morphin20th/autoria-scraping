09ee48373d89f5f35f967642be7d1cb1b63290ff
Maksym <bondarevskiymaks@gmail.com>
2026-01-27 20:50:55 +0200

    add settings, pydantic model with validation

---
+++ src/model.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, field_validator
+
+
+class Car(BaseModel):
+    url: str
+    title: str
+    price_usd: int
+    odometer: int
+    username: str
+    phone_number: int
+    image_url: str
+    image_count: int
+    car_number: str | None
+    car_vin: str | None
+
+    @field_validator("odometer", mode="before")
+    @classmethod
+    def parse_odometer(cls, value: str) -> int:
+        num = int(value.split()[0])
+        return num * 1000
+
+    @field_validator("phone_number", mode="before")
+    @classmethod
+    def parse_phone_number(cls, value: str) -> int:
+        value = value.replace("(", "").replace(")", "").replace(" ", "")
+        value = "38" + value
+        return int(value)
+
+    @field_validator("price_usd", mode="before")
+    @classmethod
+    def parse_price_usd(cls, value: str) -> int:
+        value = value.replace(" ", "").replace("$", "").replace("\xa0", "")
+        return int(value)
