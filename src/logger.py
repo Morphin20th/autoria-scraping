23d2b379d65978ae6180da803488411e14483d3b
Maksym <bondarevskiymaks@gmail.com>
2026-02-01 01:58:40 +0200

    update db scheme & model

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ import sys
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(levelname)s|\t%(message)s\t| %(asctime)s",
+    format="%(levelname)s\t|%(message)s\t\t| %(asctime)s",
     handlers=[logging.StreamHandler(sys.stdout)],
 )
 
--- src/model.py
+++ src/model.py
@@ -1,27 +1,35 @@
 from pydantic import BaseModel, field_validator
 
+from src.logger import logger
+
 
 class Car(BaseModel):
     url: str
-    title: str
-    price_usd: int
-    odometer: int
-    username: str
-    phone_number: str
-    image_url: str
-    image_count: int
-    car_number: str | None
-    car_vin: str | None
+    title: str | None = None
+    price_usd: int = 0
+    odometer: int = 0
+    username: str | None = None
+    phone_number: str | None = None
+    image_url: str | None = None
+    image_count: int = 0
+    car_number: str | None = None
+    car_vin: str | None = None
 
     @field_validator("odometer", mode="before")
     @classmethod
     def parse_odometer(cls, value: str) -> int:
+        if not value:
+            logger.warning("Failed to extract odometer")
+            return 0
         num = value.split()[0]
         return 0 if num.lower() == "без" else int(num) * 1000
 
     @field_validator("phone_number", mode="before")
     @classmethod
-    def parse_phone_number(cls, value: str) -> str:
+    def parse_phone_number(cls, value: str | None) -> str | None:
+        if not value:
+            logger.warning("Failed to extract phone number")
+            return None
         value = value.replace("(", "").replace(")", "").replace(" ", "")
         value = "38" + value
         return value
@@ -29,5 +37,11 @@ class Car(BaseModel):
     @field_validator("price_usd", mode="before")
     @classmethod
     def parse_price_usd(cls, value: str) -> int:
-        value = value.replace(" ", "").replace("$", "").replace("\xa0", "")
-        return int(value)
+        if not value:
+            logger.warning("Failed to extract price_usd")
+            return 0
+        try:
+            clean_value = "".join(filter(str.isdigit, value))
+            return int(clean_value) if clean_value else 0
+        except ValueError:
+            return 0
